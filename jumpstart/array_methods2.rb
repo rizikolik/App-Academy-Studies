6a470e12a851dec857c7285f912d2182d2f64aae
rizikolik <ynsture@gmail.com>
2019-08-24 23:42:27 +0300

    added array methods2

---
+++ jumpstart/array_methods2.rb
@@ -0,0 +1,22 @@
+arr=[1,2,3,4,5]
+
+# Push Method dfiffers from the << method .It can push a lot of elements to an array .
+
+print arr
+puts
+
+arr.push("hello","how are you?")
+print arr
+puts
+#Pop Method remove the last item in an array .
+arr.pop()
+print arr
+puts
+ #UNshift Method add a new item to the beginning of an array
+ arr.unshift("hello world!")
+ print arr
+puts
+#Shift method deletes the first item from an array
+arr.shift()
+print arr
+puts
