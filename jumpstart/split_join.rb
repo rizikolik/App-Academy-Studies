9a72cc5956b5f577143ac9df90fa2605f5fe602c
rizikolik <ynsture@gmail.com>
2019-08-27 18:01:29 -0400

    added new join and split methods

---
+++ jumpstart/split_join.rb
@@ -0,0 +1,10 @@
+#Split Method turns a String into an array and original string stays same .
+str="hello world !"
+print str.split(" ")
+puts
+#Join Method turns an array into an String 
+arr=["hello","world","!"]
+strin=arr.join(" ")
+print strin
+puts
+p arr.index("g")
