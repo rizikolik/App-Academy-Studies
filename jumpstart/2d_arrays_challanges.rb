807e6104c2308d4ff31a5dbb7256c956b648b402
rizikolik <ynsture@gmail.com>
2019-09-01 04:45:01 -0400

    added new solutions

--- jumpstart/2d_arrays_challanges.rb
+++ jumpstart/2d_arrays_challanges.rb
@@ -68,3 +68,33 @@ array_2 = [
 ]
 puts two_d_sum(array_2)    # => 15
 puts "================"
+
+# Write a method `two_d_translate` that takes in a 2 dimensional array and translates it into a 1 dimensional array.
+# You can assume that the inner arrays always have 2 elements. See the examples.
+
+def two_d_translate(arr)
+    transformed=[]
+    arr.each do |elem|
+        elem[1].times { transformed.push(elem[0])}
+        
+    end
+    return transformed
+
+end
+
+arr_1 = [
+  ['boot', 3],
+  ['camp', 2],
+  ['program', 0]
+]
+
+print two_d_translate(arr_1) # => [ 'boot', 'boot', 'boot', 'camp', 'camp' ]
+puts
+
+arr_2 = [
+  ['red', 1],
+  ['blue', 4]
+]
+
+print two_d_translate(arr_2) # => [ 'red', 'blue', 'blue', 'blue', 'blue' ]
+puts
