e845d9e982375677e56941bda6e29279a1ad6206
rizikolik <ynsture@gmail.com>
2019-09-16 23:24:06 +0300

    completed hashes

--- jumpstart/hashes/counter_hashes.rb
+++ jumpstart/hashes/counter_hashes.rb
@@ -3,3 +3,23 @@
 newly_created=Hash.new(nil)
 
 puts  newly_created["A"]==nil
+
+#Learn the Total amount of  each letter of a string with starting a empty Hash. Great Method ..
+hash=Hash.new(0) # Start the hash with a 0 initial value.
+str="hello woroolddd"
+str.each_char {|char| hash[char]+=1}
+puts hash
+
+
+##Sort By With Hashes
+
+str="hello gooood boysss  out there !"
+counter=Hash.new(0)
+
+str.each_char do |char|
+    counter[char]+=1
+end
+puts "==============="
+    puts counter
+    p counter.sort_by { |k,v| v} # This sorts the values of the result of the total count of the every letters from lower to higher .
+    p counter.sort_by { |k,v| k} #Ruby will sort the keys  alphabetically.
--- jumpstart/readme.md
+++
@@ -1,2 +0,0 @@
-# This repository is for my solutions for the App Academy's Open Full Stack Curriculum's jumpstart Challanges.
-## All the solutions made by me .
