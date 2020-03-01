1535effa2b29a2973e6fce3b4920ca74aeedbd9e
ynsture <ynsture@gmail.com>
2020-03-01 20:19:21 +0300

    started to update some Ruby

---
+++ jumpstart/hashes/ruby.rb
@@ -0,0 +1,9 @@
+#this will be first line of code of me about 4 months later
+ my_hash={
+     "name"=>"yunus",
+     "color"=>"normal",
+     "age"=>27
+ }
+ puts my_hash
+ puts my_hash["name"]
+ puts my_hash["age"]
