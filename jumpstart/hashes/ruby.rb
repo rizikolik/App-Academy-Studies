2be41df2a80ffe5c7ae8b9e71bb400a21904e4fd
ynsture <ynsture@gmail.com>
2020-03-01 20:35:31 +0300

    began to refresh some methods

--- jumpstart/hashes/ruby.rb
+++ jumpstart/hashes/ruby.rb
@@ -1,4 +1,5 @@
 #this will be first line of code of me about 4 months later
+# hashe's keys are case sensitive cause they are strings .
  my_hash={
      "name"=>"yunus",
      "color"=>"normal",
@@ -7,3 +8,16 @@
  puts my_hash
  puts my_hash["name"]
  puts my_hash["age"]
+
+
+ new_hash={
+    "name"=>"sdad",
+    "color"=>"normal",
+    "age"=>27,
+    "likes"=>["football","tennis","walk"]
+ }
+  new_hash["location"]="Turkey"   ## this will add a new key value pair to the hash .
+ puts new_hash.length  ## this will give the number of the keys in an array .
+ puts new_hash
+
+ puts  new_hash.has_key?("age") # usefull when we are searching a big hash .
