e485534516280e208f42a8d1675ac584faf1dc1d
rizikolik <ynsture@gmail.com>
2020-03-14 13:54:07 +0000

    began to referances challanges

---
+++ Software Foundations/scope_referances/referances_challange.rb
@@ -0,0 +1,119 @@
+# Write a method, grid(n, m), that accepts two numbers as args.
+# The method should return a 2D array with n rows and m columns (this means that there are n subarrays, each of length m).
+
+def grid(n, m)
+arr=Array.new(n) {Array.new(m)}
+	return arr
+end
+
+result_1 = grid(2, 3)
+p result_1              # => [[nil, nil, nil], [nil, nil, nil]]
+result_1[0][0] = "X"
+p result_1              # => [["X", nil, nil], [nil, nil, nil]]
+
+
+result_2 = grid(4, 2)
+p result_2              # => [[nil, nil], [nil, nil], [nil, nil], [nil, nil]]
+result_2[0][0] = "Q"
+p result_2              # => [["Q", nil], [nil, nil], [nil, nil], [nil, nil]] 
+
+puts "================"
+
+# Write a method, ele_replace!(array, hash), that accepts an array and a hash as args.
+# The method should mutate the given array by replacing elements with their corresponding values in the hash.
+# The method should return the given array.
+#
+# Note: this method must MUTATE the input array. This means that the object_id of the input array should be identical
+# to the object_id of the returned array. The exact object_ids you get back don't matter. We just want the ids
+# to be the same before and after calling your method.
+
+def ele_replace!(array, hash)
+	array.each do |k|
+		if hash.has_key?(k)
+		k=hash[k]
+		else k
+		end
+		
+	end
+	return array
+end
+
+
+array_1 = [4, 2, 0, 2]
+p array_1.object_id         # => 70119569670520
+result_1 = ele_replace!(array_1, {2=>"two", 0=>"zero", 5=>"five"})
+p result_1                  # => [4, "two", "zero", "two"]
+p result_1.object_id        # => 70119569670520
+
+
+array_2 = ["Matthias", "Simcha", "Mashu", "David"]
+p array_2.object_id         # => 70119569668160
+result_2 = ele_replace!(array_2, "Matthias"=>"J", "Mashu"=>"D")
+p result_2                  # => ["J", "Simcha", "D", "David"]
+p result_2.object_id        # => 70119569668160
+puts "============="
+
+
+# Write a method, my_rotate!(array, amt), that accepts an array and a number as args.
+# The method should mutate the array by rotating the elements 'amt' number of times. 
+# When given a positive 'amt', a single rotation will rotate left, causing the first element to move to the last index
+# When given a negative 'amt', a single rotation will rotate right, causing the last element to move to the first index
+# The method should return the given array.
+# Do not use the built-in Array#rotate
+# 
+# Note: this method must MUTATE the input array. This means that the object_id of the input array should be identical
+# to the object_id of the returned array. The exact object_ids you get back don't matter. We just want the ids
+# to be the same before and after calling your method.
+
+
+def my_rotate!(array, amt)
+
+	if amt<0
+		arr=[]
+array.each.with_index  do |el,idx|
+		if  idx+amt.abs<array.length
+				if !arr.include?(idx+amt.abs)
+				array[idx+amt.abs]=el
+					arr << (idx+amt.abs)
+				else 
+					el 
+				end
+		else 
+				if !arr.include?((idx+amt.abs)-(array.length))
+				array[(idx+amt.abs)-(array.length)]=el 
+				arr << ((idx+amt.abs)-(array.length))
+				else
+					el 
+				end
+			
+	end
+
+	end
+end
+		
+	return array
+end
+
+
+array_1 = ["a", "b", "c", "d"]
+p array_1.object_id                 # => 70354216023780
+#result_1 = my_rotate!(array_1, 2)
+#p result_1                          # => ["c", "d", "a", "b"]
+#p result_1.object_id                # => 70354216023780
+
+
+array_2 = ["NOMAD", "SOHO", "TRIBECA"]
+#p array_2.object_id                 # => 70354216019660
+#result_2 = my_rotate!(array_2, 1)
+#p result_2                          # => ["SOHO", "TRIBECA", "NOMAD"]
+#p result_2.object_id                # => 70354216019660
+
+
+array_3 = ["a", "b", "c", "d"]
+p array_3.object_id                 # => 70354216016500
+result_3 = my_rotate!(array_3, -3)
+p result_3                          # => ["b", "c", "d", "a"]
+p result_3.object_id                # => 70354216016500
+
+
+
