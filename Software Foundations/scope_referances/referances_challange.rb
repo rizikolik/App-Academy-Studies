fd5c333f9f6f0a3647bdb948745a73c17ca0750f
ynsture <ynsture@gmail.com>
2020-03-15 12:33:27 +0300

    completed referances and scopes challanges

--- Software Foundations/scope_referances/referances_challange.rb
+++ Software Foundations/scope_referances/referances_challange.rb
@@ -67,46 +67,34 @@ puts "============="
 
 
 def my_rotate!(array, amt)
-
-	if amt<0
-		arr=[]
-array.each.with_index  do |el,idx|
-		if  idx+amt.abs<array.length
-				if !arr.include?(idx+amt.abs)
-				array[idx+amt.abs]=el
-					arr << (idx+amt.abs)
-				else 
-					el 
-				end
-		else 
-				if !arr.include?((idx+amt.abs)-(array.length))
-				array[(idx+amt.abs)-(array.length)]=el 
-				arr << ((idx+amt.abs)-(array.length))
-				else
-					el 
-				end
-			
+if amt <0
+	amt.abs.times do
+		el = array.pop
+		array.unshift(el)
 	end
-
+else
+	amt.times do 
+	el=array.shift()
+	array << el
+	
 	end
 end
-		
-	return array
+return array
 end
 
 
 array_1 = ["a", "b", "c", "d"]
 p array_1.object_id                 # => 70354216023780
-#result_1 = my_rotate!(array_1, 2)
-#p result_1                          # => ["c", "d", "a", "b"]
-#p result_1.object_id                # => 70354216023780
+result_1 = my_rotate!(array_1, 2)
+p result_1                          # => ["c", "d", "a", "b"]
+p result_1.object_id                # => 70354216023780
 
 
 array_2 = ["NOMAD", "SOHO", "TRIBECA"]
-#p array_2.object_id                 # => 70354216019660
-#result_2 = my_rotate!(array_2, 1)
-#p result_2                          # => ["SOHO", "TRIBECA", "NOMAD"]
-#p result_2.object_id                # => 70354216019660
+p array_2.object_id                 # => 70354216019660
+result_2 = my_rotate!(array_2, 1)
+p result_2                          # => ["SOHO", "TRIBECA", "NOMAD"]
+p result_2.object_id                # => 70354216019660
 
 
 array_3 = ["a", "b", "c", "d"]
@@ -114,6 +102,68 @@ p array_3.object_id                 # => 70354216016500
 result_3 = my_rotate!(array_3, -3)
 p result_3                          # => ["b", "c", "d", "a"]
 p result_3.object_id                # => 70354216016500
+puts "=================="
 
 
+# Write a method, scalar_multiple!(array, multiplier), that accepts an array and a number as args. 
+# The method should mutate the given array by multiplying all of the elements by the multiplier.
+# The method should return the given array.
+# 
+# Note: this method must MUTATE the input array. This means that the object_id of the input array should be identical
+# to the object_id of the returned array. The exact object_ids you get back don't matter. We just want the ids
+# to be the same before and after calling your method.
 
+def scalar_multiple!(array, multiplier)
+array.each.with_index do |k,idx|
+
+
+	newvalue=k*multiplier
+	array[idx]=newvalue
+end
+return array
+end
+
+
+array_1 = [7, 0, 4]
+p array_1.object_id                             # => 70326264380840
+result_1 = scalar_multiple!(array_1, 3)
+p result_1                                      # => [21, 0, 12]
+p result_1.object_id                            # => 70326264380840
+
+
+array_2 = [90, 30, 4, 12]
+p array_2.object_id                             # => 70326264378100
+result_2 = scalar_multiple!(array_2, 0.5)
+p result_2                                      # => [45.0, 15.0, 2.0, 6.0]
+p result_2.object_id                            # => 70326264378100
+
+puts "=================="
+
+# Write a method, replace_char_at!(str, char, idx), that accepts a string, character, and an index as arguments. 
+# The method should mutate the str by replacing the given idx of the str with the char. The method
+# should return the str.
+# 
+# Note: this method must MUTATE the input str. This means that the object_id of the input string should be identical
+# to the object_id of the returned string. The exact object_ids you get back don't matter. We just want the ids
+# to be the same before and after calling your method.
+
+def replace_char_at!(str, char, idx)
+str[idx]=char
+return str
+
+
+end
+
+
+str_1 = "hello"
+p str_1.object_id                       # => 70120944788380
+result_1 = replace_char_at!(str_1, "j", 0)
+p result_1                              # => "jello"
+p result_1.object_id                    # => 70120944788380
+
+
+str_2 = "world"
+p str_2.object_id                       # => 70120944769940
+result_2 = replace_char_at!(str_2, "!", 2) 
+p result_2                              # => "wo!ld"
+p result_2.object_id                    # => 70120944769940
