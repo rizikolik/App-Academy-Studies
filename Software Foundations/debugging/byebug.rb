cff76be3379cb36214f55d83ee30f859275349d8
ynsture <ynsture@gmail.com>
2020-03-22 23:11:50 +0300

    added byebug news

--- Software Foundations/debugging/byebug.rb
+++ Software Foundations/debugging/byebug.rb
@@ -1,22 +1,9 @@
-# code.rb
- require "byebug"   #
-2   
-  def is_prime?(number)
-    (2...number).each do |factor|
-      return false if number % factor == 0
-    end
-
-    number > 1
-  end
-
- def first_n_primes(num_primes)
-   primes = []
-   num = 2
-   debugger        #
-   while primes.length < num_primes
-     primes << num if is_prime?(num)
-     num += 1
-   end
-   primes
- end
- p first_n_primes(11)
+require ("byebug")
+def average(num1,num2)
+  total=num1+num2
+  total/2
+end
+debugger
+first=average(10,20)
+second=first+0.5
+p second
