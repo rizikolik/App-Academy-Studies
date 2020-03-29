ce47bd99389983409f82bf5c44bfb50a8e9d392f
ynsture <ynsture@gmail.com>
2020-03-29 23:33:06 +0300

    completed first oop section

--- Software Foundations/classes/class_variables.rb
+++ Software Foundations/classes/class_variables.rb
@@ -1,1 +1,20 @@
 
+class Computer
+    @@users={}
+    def initialize(username,password)
+      @username=username
+      @password=password
+      @@users[username]=password
+      @files={}
+    end
+    def create(filename)
+      time=Time.now
+    
+      @files[filename]=time
+      puts "New file created"
+    end
+    def Computer.get_users
+      @@users
+    end
+  end
+  my_computer=Computer.new("usernew",12345)
