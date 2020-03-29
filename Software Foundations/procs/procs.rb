d7ccf4931c6ac85e7ed8784f0cfc59ebd8348c68
ynsture <ynsture@gmail.com>
2020-03-29 22:20:39 +0300

    restudying on procs and lambdas

--- Software Foundations/procs/procs.rb
+++ Software Foundations/procs/procs.rb
@@ -18,3 +18,20 @@ def add_and_proc(num_1, num_2, prc)
 
   doubler = Proc.new { |num| num * 2 }
 p [1,2,3].map(&doubler) # => [2, 4, 6]  # Converting a prob and method each other with &
+
+## Some useful infos about procs and lambdaS
+def batman_ironman_proc
+  victor = Proc.new { return "Batman will win!" }
+  victor.call
+  "Iron Man will win!"
+end
+
+puts batman_ironman_proc
+
+def batman_ironman_lambda
+  victor = lambda { return "Batman will win!" }
+  victor.call
+  "Iron Man will win!"
+end
+
+puts batman_ironman_lambda
