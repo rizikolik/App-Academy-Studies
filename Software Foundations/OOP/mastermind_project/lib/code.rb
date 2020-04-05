8ffaeef38ee69c98ffc35ed453377e02a4c7fb46
ynsture <ynsture@gmail.com>
2020-04-05 11:36:19 +0300

    completed MAsterMind Project

--- Software Foundations/OOP/mastermind_project/lib/code.rb
+++ Software Foundations/OOP/mastermind_project/lib/code.rb
@@ -1,9 +1,62 @@
 class Code
-  POSSIBLE_PEGS = {
-    "R" => :red,
-    "G" => :green,
-    "B" => :blue,
-    "Y" => :yellow
-  }
+      attr_reader:pegs
+      POSSIBLE_PEGS = {
+        "R" => :red,
+        "G" => :green,
+        "B" => :blue,
+        "Y" => :yellow
+      }
+     
+      def initialize(pegs)
+      if Code.valid_pegs?(pegs)
+        @pegs = pegs.map(&:upcase)
+      else
+        raise "There was an error"
+      end
+      end
+    
+      def  self.valid_pegs?(array)
+   
+      array.all?{|ele| Code::POSSIBLE_PEGS.include?(ele.upcase)}
+      end
+
+      def self.random(length)
+      rando= Array.new(length) { Code::POSSIBLE_PEGS.keys.sample }
+        Code.new(rando)
+      end
+      
+      def self.from_string(str)
+        arr=str.split("")
+        Code.new(arr)
+      end
+
+      def [](index)
+      @pegs[index]
+      end
+
+      def length
+        @pegs.length
+      end
+      
+      def num_exact_matches(instanc)
+        num=0
+        instanc.pegs.each.with_index do |key,i|
+            if @pegs[i]==key
+              num+=1
+            end
+        end
+        num
+      end
+
+      def num_near_matches(instanc)
+        count=instanc.pegs.count{|ele| @pegs.include?ele}
+       count- num_exact_matches(instanc)
+                     
+      end
+
+      def ==(other_code)
+        self.length == other_code.length && self.num_exact_matches(other_code) == self.length
+      end
+
 
 end
--- Software Foundations/OOP/mastermind_project/lib/mastermind.rb
+++ Software Foundations/OOP/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,22 @@
 require_relative "code"
 
 class Mastermind
+   def initialize(length)
+   @secret_code=Code.random(length)
+   end
+
+   def print_matches(code)
+    puts "exact matches: #{@secret_code.num_exact_matches(code)}"
+    puts "near matches: #{@secret_code.num_near_matches(code)}"
+   
+   end
+
+   def ask_user_for_guess
+   print 'Enter a code'
+   guess=Code.from_string(gets.chomp)
+ self.print_matches(guess)
+ @secret_code==guess
+
+   end
 
 end
