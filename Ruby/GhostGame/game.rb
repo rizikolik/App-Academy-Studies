cb511d195e0934a1ff846d411c0c1d0470208464
ynsture <ynsture@gmail.com>
2020-04-12 12:19:26 +0300

    began to Html and Css section

---
+++ Ruby/GhostGame/game.rb
@@ -0,0 +1,21 @@
+require_relative "player"
+  class Game
+
+     def initialize()
+            @players
+            @fragment
+            @dictionary={}
+        end
+        def  play_round
+        end
+
+        def current_player
+        end
+        def previous_player
+        end
+        def next_player!
+        end
+
+        def take_turn(player)
+        end
+end
