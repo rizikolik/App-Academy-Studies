e4511c70f12c2352ad8f52b9c7d5991a6082bc44
ynsture <ynsture@gmail.com>
2020-04-27 21:44:30 +0300

    sqlite3 orm

--- SQL/ORM/play.rb
+++ SQL/ORM/play.rb
@@ -1,5 +1,21 @@
+require('sqlite3')
+require('singleton')
+
+class PlayDBConnection < SQLite3 ::Database
+    include singleton
+
+    def initialize
+        super(plays.db)
+
+    end
+end
+
+
+
+
 clasS PLay
-::all 
+def self.all 
+end
 def initialize
 end
 def create
