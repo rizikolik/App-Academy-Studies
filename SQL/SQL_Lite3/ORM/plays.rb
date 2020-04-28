4b136a6071ffeeb7661b6042b1e690104fe8e0db
ynsture <ynsture@gmail.com>
2020-04-28 23:58:02 +0300

    completed ORM exam

--- SQL/SQL_Lite3/ORM/plays.rb
+++ SQL/SQL_Lite3/ORM/plays.rb
@@ -26,10 +26,15 @@ attr_accessor :title , :year , :playwright_id
         data.map{ |datum|  Play.new(datum)}
     end
     def self.find_by_title(title)
+        result=PlayDBConnection.instance.execute(<<-SQL,title)
+        SELECT*FROM plays WHERE title=?
+        SQL
     end
 
     def self.find_by_playwright(name)
-
+        result=PlayDBConnection.instance.execute(<<-SQL,name)
+        SELECT * FROM plays JOIN playwrights ON plays.playwright_id = playwrights.id WHERE name = ?
+        SQL
     end
     def initialize(options)
         @id=options['id']
@@ -67,15 +72,48 @@ end
 
 class Playwright
     def self.all
+        data=PlayDBConnection.instance.execute("SELECT*FROM playwrights")
+        data.map{|datum| Playwright.new(datum)}
     end
     def self.find_by_name(name)
+        result=PlayDBConnection.instance.execute(<<-SQL,name)
+        SELECT*FROM playwrights WHERE name=?
+        SQL
     end
-    def new
+    def initialize(options)
+        @id=options['id']
+        @name=options['name']
+        @birth_year=options['birth_year']
     end
      def create
+        raise "#{self} is on the list already.." if @id
+        PlayDBConnection.instance.execute(<<-SQL,@name,@birth_year)
+        INSERT INTO
+        playwrights
+        (name,birth_year)
+        VALUES
+        (?,?)
+       
+
+        SQL
+        @id=PlayDBConnection.instance.last_insert_row_id
      end
      def update
+        raise "#{self} is NOT on the list ." unless @id
+        PlayDBConnection.instance.execute(<<-SQL,@name,@birth_year,id)
+        UPDATE
+        playwrights
+        SET
+        name=?,birth_year=?
+        WHERE
+        id=?
+        SQL
+        self
      end
      def get_plays
+        plays=PlayDBConnection.instance.execute(<<-SQL,@id)
+        SELECT*FROM plays WHERE playwrights_id=?
+        SQL
+        plays.map{|play| Play.new(play)}
      end
-     
+
