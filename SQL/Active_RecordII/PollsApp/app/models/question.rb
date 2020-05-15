594c5c1c554a6d8a0fd00e3d035af01f121bf8b9
ynsture <ynsture@gmail.com>
2020-05-15 22:37:56 +0300

    end of project

--- SQL/Active_RecordII/PollsApp/app/models/question.rb
+++ SQL/Active_RecordII/PollsApp/app/models/question.rb
@@ -6,4 +6,40 @@ class Question < ApplicationRecord
     has_many :responses,
     through: :answer_choices,
     source: :responses
+    def results_n_plus_1
+        # N+1 way:
+    results = {}
+    self.answer_choices.each do |ac|
+      results[ac.text] = ac.responses.count
+    end
+    results
+    end
+
+    def results_2_queries
+        # 2-queries; all responses transferred:
+        results = {}
+        self.answer_choices.includes(:responses).each do |ac|
+          results[ac.text] = ac.responses.length
+        end
+        results
+      end
+      def results_sql_version
+         # 1-query way all SQL
+            acs = AnswerChoice.find_by_sql([<<-SQL, id])
+              SELECT
+                answer_choices.text, COUNT(responses.id) AS num_responses
+              FROM
+                answer_choices
+                LEFT OUTER JOIN responses
+                  ON answer_choices.id = responses.answer_choice_id
+              WHERE
+                answer_choices.question_id = ?
+              GROUP BY
+                answer_choices.id
+            SQL
+        
+            acs.inject({}) do |results, ac|
+              results[ac.text] = ac.num_responses; results
+            end
+          end
 end
--- SQL/Active_RecordII/PollsApp/app/models/response.rb
+++ SQL/Active_RecordII/PollsApp/app/models/response.rb
@@ -12,4 +12,19 @@ class Response < ApplicationRecord
   has_one :question,
    through: :answer_choice,
    source: :question
+
+
+   def sibling_responses
+  self.question.responses.where.not(id: self.id)
+   end
+
+   def respondent_already_answered?
+   sibling_responses.exists?(respondent_id: self.respondent_id)
+   end
+   def not_duplicate_response
+    if respondent_already_answered?
+      errors[:respondent_id] << 'cannot vote twice for question'
+    end
+
+   end
 end
