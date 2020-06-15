2401d013aae6b382c337a80be36d88e5e0702362
ynsture <ynsture@gmail.com>
2020-06-15 23:15:22 +0300

    updated app with new model

--- Rails/Rentanimal/app/models/cat.rb
+++ Rails/Rentanimal/app/models/cat.rb
@@ -1,2 +1,10 @@
 class Cat < ApplicationRecord
+    validates :size, inclusion: { in: %w(small medium large),
+        message: "%{value} is not a valid size" }
+    def initialize(birth_date)
+        @birth_date=birth_date
+    def age
+        Time.now.year -@birth_date.year
+    end
+
 end
