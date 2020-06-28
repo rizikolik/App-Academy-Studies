819bfb4a91d73ba6af362c35d67eece309864e4a
ynsture <ynsture@gmail.com>
2020-06-28 23:56:35 +0300

    updated user controllers and models

--- Rails/AuthTest/app/models/user.rb
+++ Rails/AuthTest/app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, presence: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true }
+    def password=(password)
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
+      end
+      def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+      end
 end
