1ecfac646ec2f628dc7d3e8a35c7be509134711e
ynsture <ynsture@gmail.com>
2020-06-29 21:16:54 +0300

    updated models and controllers

--- Rails/AuthTest/app/controllers/sessions_controller.rb
+++ Rails/AuthTest/app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
-end
+    def create
+        user = User.find_by_credentials(
+          params[:user][:username],
+          params[:user][:password]
+        )
+    
+        if user.nil?
+          render json: 'Credentials were wrong'
+        else
+          render json: "Welcome back #{user.username}!"
+        end
+      end
+    
+      def new
+      end
+    end
--- Rails/AuthTest/app/models/user.rb
+++ Rails/AuthTest/app/models/user.rb
@@ -4,11 +4,30 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
   validates :password, length: { minimum: 6, allow_nil: true }
+  validates :session_token, presence: true
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
     def password=(password)
       @password = password
       self.password_digest = BCrypt::Password.create(password)
       end
       def is_password?(password)
         BCrypt::Password.new(self.password_digest).is_password?(password)
+      end
+      def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+      end
+    
+      private
+      def ensure_session_token
+        # we must be sure to use the ||= operator instead of = or ||, otherwise
+        # we will end up with a new session token every time we create
+        # a new instance of the User class. This includes finding it in the DB!
+        self.session_token ||= self.class.generate_session_token
       end
 end
